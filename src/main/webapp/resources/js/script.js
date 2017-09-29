b13a1585b992972ceedfe3d9087bde0aeb6a4295
Eric Ribeiro <ericribeiro@outlook.com.br>
2017-09-29 05:54:11 -0300

    Cadastro de empresas refeito com Materialize.css

---
+++ src/main/webapp/resources/js/script.js
@@ -0,0 +1,9 @@
+$('.button-collapse').sideNav({
+      menuWidth: 300, 
+      edge: 'right',
+      closeOnClick: true, 
+      draggable: true, 
+      onOpen: function(el) {},
+      onClose: function(el) {},
+  }
+);
