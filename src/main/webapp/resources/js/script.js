55fab4a0f4b91d802882c34b73b550739ab4dd78
Eric Ribeiro <ericribeiro@outlook.com.br>
2017-10-01 02:59:27 -0300

    cadastro de vagas totalmente migrado para o Materialize CSS

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,59 +1,69 @@
-jQuery(function($) {
-	$('textarea').keyup(function() {
-		var maxLength = 120;
-		var length = $(this).val().length;
-		var length = maxLength-length;
-		$('.character-counter').text(length + "/" + maxLength);
-	});
-	
-	$('textarea').focus(function() {
-		$('.character-counter').show();
-		
-	});
-	
-	$('textarea').blur(function() {
-		$('.character-counter').hide();
-	});
+jQuery(function ($) {
+    countCharInTextArea();
+    datePicker();
+    hideEmptySalaryRange();
 });
 
-jQuery(function($) {
-	
-//	$('.button-collapse').sideNav({
-//	      menuWidth: 300, 
-//	      edge: 'right',
-//	      closeOnClick: true, 
-//	      draggable: true, 
-//	      onOpen: function(el) {},
-//	      onClose: function(el) {},
-//	  }
-//	);
-});
+function countCharInTextArea() {
+    $('textarea').keyup(function () {
+        var maxLength = 120;
+        var length = $(this).val().length;
+        var length = maxLength - length;
+        $('.character-counter').text(length + "/" + maxLength);
+    });
 
-jQuery(function($) {
- $('.datepicker').pickadate({
-	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15, // Creates a dropdown of 15 years to control year,
-	    today: 'Today',
-	    clear: 'Clear',
-	    close: 'Ok',
-	    closeOnSelect: false // Close upon selecting a date,
-  });
-});
+    $('textarea').focus(function () {
+        $('.character-counter').show();
 
-jQuery(function($) {
-	var slider = document.getElementById('test-slider');
-	noUiSlider.create(slider, {
-	 start: [20, 80],
-	 connect: true,
-	 step: 1,
-	 orientation: 'horizontal', // 'horizontal' or 'vertical'
-	 range: {
-	   'min': 0,
-	   'max': 100
-	 },
-	 format: wNumb({
-	   decimals: 0
-	 })
-	});
-});
+    });
+
+    $('textarea').blur(function () {
+        $('.character-counter').hide();
+    });
+};
+
+function datePicker()   {
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 5, // Creates a dropdown of 5 years to control year,
+        today: 'Hoje',
+        clear: 'Limpar',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+};
+
+function slider() {
+    var slider = document.getElementById('test-slider');
+    noUiSlider.create(slider, {
+        start: [20, 80],
+        connect: true,
+        step: 1,
+        orientation: 'horizontal', // 'horizontal' or 'vertical'
+        range: {
+            'min': 0,
+            'max': 100
+        },
+        format: wNumb({
+            decimals: 0
+        })
+    });
+}
+
+function hideEmptySalaryRange() {
+    if ($('.salaryRange').val() == 0.0)
+        $('.salaryRange').val("");
+};
+
+function sideNav() {
+    $('.button-collapse').sideNav({
+            menuWidth: 300,
+            edge: 'right',
+            closeOnClick: true,
+            draggable: true,
+            onOpen: function(el) {},
+            onClose: function(el) {},
+        }
+    );
+};
 
