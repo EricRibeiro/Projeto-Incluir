2bc9b454c06edff401a45d7140c9806d8e7b9192
Carlos Felipe <carlosfelipearantes@gmail.com>
2017-10-19 06:37:13 -0200

    Primeira implementacao funcional do mapa. Teste de marcadores e localizacao atual.

---
+++ src/main/java/org/tis/bean/MapaBean.java
@@ -0,0 +1,37 @@
+package org.tis.bean;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+ 
+@ManagedBean
+public class MapaBean implements Serializable {
+    
+    private MapModel simpleModel;
+    
+    @PostConstruct
+    public void init() {
+        simpleModel = new DefaultMapModel();
+          
+        //Shared coordinates
+        LatLng coord1 = new LatLng(36.879466, 30.667648);
+        LatLng coord2 = new LatLng(36.883707, 30.689216);
+        LatLng coord3 = new LatLng(36.879703, 30.706707);
+        LatLng coord4 = new LatLng(36.885233, 30.702323);
+          
+        //Basic marker
+        simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
+        simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
+        simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));
+        simpleModel.addOverlay(new Marker(coord4, "Kaleici"));
+    }
+  
+    public MapModel getSimpleModel() {
+        return simpleModel;
+    }
+}
