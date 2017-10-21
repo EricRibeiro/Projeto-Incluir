5468340b6268f5ae8d9a545bdc85c31447465d34
Carlos Felipe <carlosfelipearantes@gmail.com>
2017-10-21 04:09:31 -0200

    Correção do backend de mapa e adição no fluxo de pessoa no template-pessoa

--- src/main/java/org/tis/bean/MapaBean.java
+++ src/main/java/org/tis/bean/MapaBean.java
@@ -27,15 +27,60 @@ public class MapaBean implements Serializable {
     public void init() {
        
     	simpleModel = new DefaultMapModel();
-    	
-        ArrayList <LatLng> coordenadas = new ArrayList<LatLng>();
-        
+    	        
         List<Vaga> vagas = vagaDao.vagasAbertasPessoa();
+        
+        ArrayList <String> nomes = new ArrayList<String>();
+        
+        int i = 0;
+        
+        for (Vaga e: vagas ) {
+        	
+        	nomes.add(e.getEmpresa().getRazaoSocial());
+        	
+        }
         
         for ( Vaga e : vagas ) {
         	
-        		simpleModel.addOverlay(new Marker ( new LatLng( Double.valueOf(e.getEmpresa().getLatitude() ) , Double.valueOf( e.getEmpresa().getLongitude() ) ) , e.getCargo() ) );
+        	//TENTATIVAS DE USAR FUNCAO LAMBDA PARA FILTAR E ENCONTRAR O VALOR DE STRING IGUAL DIRETO NA COLLECTION
+    		//vagas.stream().anyMatch( String -> e.getEmpresa().getRazaoSocial().equalsIgnoreCase(s)
+    		//nomes.stream().filter( str -> str.trim().equals(s+"") ) != null 
+    		//nomes.toString().contains(e.getEmpresa().getRazaoSocial())
+        	// 
+        	//IDEIA NOVA : USAR UM MAPA PARA MAPEAR UMA STRING (nome da empresa ) para uma collection de marcadores e iterar neles.
+        	//
+        	// MAIS FACIL IMPLEMENTADA COM UM ITERATOR: 
+        	
+        	i = 0;
+        	
+        	for (String n : nomes ) {
         		
+        		if (n.equalsIgnoreCase(e.getEmpresa().getRazaoSocial())) {
+        			
+        			i++;
+        			
+        		}
+        		
+        	}
+        	// nao trata vaga em duplicidade. precisa-se mapear.
+        	if ( true ){
+        		 
+        		String dados =  e.getEmpresa().getRazaoSocial() + " : \r\n" 
+ 							 + "Vaga: " + e.getCargo() +  "\r\n"
+ 							 + "Endereço:\r\n"
+ 							 + e.getEmpresa().getLogradouro() +  " " 
+ 							 + e.getEmpresa().getNumero() + "\r\n"
+ 							 + e.getEmpresa().getBairro() + "\r\n"
+ 							 + e.getEmpresa().getCep();
+        		
+        		simpleModel.addOverlay(new Marker ( new LatLng( Double.valueOf(e.getEmpresa().getLatitude() ) , Double.valueOf( e.getEmpresa().getLongitude() ) ) 
+       				   , dados ));
+         		 	
+        	}
+        	else {
+        	
+        	
+        	}
         }
 
     }
@@ -44,21 +89,14 @@ public class MapaBean implements Serializable {
         return simpleModel;
     }
     
-    public void onGeocode(GeocodeEvent event) {
+    @SuppressWarnings("unused")
+	public void onGeocode(GeocodeEvent event) {
     	 List<GeocodeResult> results = event.getResults();
     }
     
-    public ArrayList montarVagas(){
+    public ArrayList<Vaga> montarVagas(){
     	List<Vaga> vagas = vagaDao.vagasAbertasPessoa();
     	String endereco;
-    	for( Vaga e : vagas ){
-    		endereco = null;
-    		endereco = e.getEmpresa().getLogradouro() + " ";
-    		endereco+= e.getEmpresa().getNumero() + " ";
-    		endereco+= e.getEmpresa().getBairro() + " ";
-    		endereco+= e.getEmpresa().getMunicipio() + " ";
-    	}
-    	
     	return (ArrayList) vagas;
     }
 }
