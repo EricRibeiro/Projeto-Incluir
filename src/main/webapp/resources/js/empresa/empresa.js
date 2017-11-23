6bb963af746dcf3fb9bd2a7091f23d950bee3735
Eric Ribeiro <ericribeiro@outlook.com.br>
2017-11-23 15:56:57 -0200

    Validação de CNPJ implementada

--- src/main/webapp/resources/js/empresa/empresa.js
+++ src/main/webapp/resources/js/empresa/empresa.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
     onKeyDownAddMasks();
+    onKeyOutCheckCNPJ()
     setLocationCoordinates();
     getAddress();
 });
@@ -21,10 +22,25 @@ var $numero = $("input[id*='numero']");
 
 function onKeyDownAddMasks() {
     var $cnpj = $("input[id*='cnpj']");
+
     $cnpj.keydown(function () {
         $cnpj.mask("99.999.999/9999-99");
     });
 };
+
+function onKeyOutCheckCNPJ() {
+    var $cnpj = $("input[id*='cnpj']");
+
+    $cnpj.blur(function () {
+       if(!isCNPJValid($(this).val())) {
+           $(this)
+               .val("")
+               .addClass('invalid')
+               .next()
+               .addClass('active');
+       }
+    });
+}
 
 function setLocationCoordinates() {
     var geocoder = new google.maps.Geocoder();
@@ -130,3 +146,58 @@ function onGetAddressError() {
     $addressLabel
         .addClass('active');
 };
+
+function isCNPJValid(cnpj) {
+
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj == '') return false;
+
+    if (cnpj.length != 14)
+        return false;
+
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" ||
+        cnpj == "11111111111111" ||
+        cnpj == "22222222222222" ||
+        cnpj == "33333333333333" ||
+        cnpj == "44444444444444" ||
+        cnpj == "55555555555555" ||
+        cnpj == "66666666666666" ||
+        cnpj == "77777777777777" ||
+        cnpj == "88888888888888" ||
+        cnpj == "99999999999999")
+        return false;
+
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0, tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+        return false;
+
+    return true;
+
+}
+
